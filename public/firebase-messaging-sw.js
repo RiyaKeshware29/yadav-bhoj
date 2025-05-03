5244cca14f559df3e9da1fa09779a25f5bbb4aac
Riya Keshware <riyak@silveroakhealth.com>
2025-05-03 21:11:45 +0530

    env var removed

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBOJWe8jU3RfoqZPuoHvB5ZJeaeQCzMOgU",
+  authDomain: "fir-food-project-6fb6e.firebaseapp.com",
+  projectId: "fir-food-project-6fb6e",
+  storageBucket: "fir-food-project-6fb6e.firebasestorage.app",
+  messagingSenderId: "617071619767",
+  appId: "1:617071619767:web:4b379e0aa27030586a2732"
 });
 
 const messaging = firebase.messaging();
