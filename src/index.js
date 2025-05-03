c137a1c4f5c506c5318d3b8eac042cfd7af16701
Riya Keshware <riyak@silveroakhealth.com>
2025-05-03 17:45:17 +0530

    Resolved merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' instead of 'react-dom'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -35,4 +17,3 @@ root.render(
   </React.StrictMode>
 );
 
->>>>>>> f9922f6a53b6d666415140d0c736a65b2e9868ef
